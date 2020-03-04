db241a87255039b0c43bab52351c0ec059ac8559
vebradev <justinas@vebra.dev>
2020-03-04 23:01:41 +0000

    fix connection of rooms

--- util/create_world.py
+++ util/create_world.py
@@ -1,46 +1,54 @@
 from django.contrib.auth.models import User
 from adventure.models import Player, Room
 
-
 Room.objects.all().delete()
 
-r_outside = Room(title="Outside Cave Entrance",
-                 description="North of you, the cave mount beckons")
+r_one = Room(title="1", description="Room 1")
+r_two = Room(title="2", description="Room 2")
+r_three = Room(title="3", description="Room 3")
+r_four = Room(title="4", description="Room 4")
+r_five = Room(title="5", description="Room 5")
+r_six = Room(title="6", description="Room 6")
+r_seven = Room(title="7", description="Room 7")
+r_eight = Room(title="8", description="Room 8")
+r_nine = Room(title="9", description="Room 9")
 
-r_foyer = Room(
-    title="Foyer", description="""Dim light filters in from the south. Dusty passages run north and east.""")
-
-r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""")
-
-r_narrow = Room(title="Narrow Passage", description="""The narrow passage bends here from west
-to north. The smell of gold permeates the air.""")
-
-r_treasure = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""")
-
-r_outside.save()
-r_foyer.save()
-r_overlook.save()
-r_narrow.save()
-r_treasure.save()
+r_one.save()
+r_two.save()
+r_three.save()
+r_four.save()
+r_five.save()
+r_six.save()
+r_seven.save()
+r_eight.save()
+r_nine.save()
 
 # Link rooms together
-r_outside.connectRooms(r_foyer, "n")
-r_foyer.connectRooms(r_outside, "s")
+r_one.connectRooms(r_two, "e")
+r_two.connectRooms(r_one, "w")
 
-r_foyer.connectRooms(r_overlook, "n")
-r_overlook.connectRooms(r_foyer, "s")
+r_one.connectRooms(r_four, "s")
+r_four.connectRooms(r_one, "n")
 
-r_foyer.connectRooms(r_narrow, "e")
-r_narrow.connectRooms(r_foyer, "w")
+r_four.connectRooms(r_five, "e")
+r_five.connectRooms(r_four, "w")
 
-r_narrow.connectRooms(r_treasure, "n")
-r_treasure.connectRooms(r_narrow, "s")
+r_five.connectRooms(r_six, "e")
+r_six.connectRooms(r_five, "w")
+
+r_three.connectRooms(r_six, "s")
+r_six.connectRooms(r_three, "n")
+
+r_five.connectRooms(r_eight, "s")
+r_eight.connectRooms(r_five, "n")
+
+r_seven.connectRooms(r_eight, "e")
+r_eight.connectRooms(r_seven, "w")
+
+r_eight.connectRooms(r_nine, "e")
+r_nine.connectRooms(r_eight, "w")
 
 players = Player.objects.all()
 for p in players:
-    p.currentRoom = r_outside.id
+    p.currentRoom = r_one.id
     p.save()
