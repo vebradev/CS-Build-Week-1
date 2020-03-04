b56e777316112aa9f6912e3fc8d631af39ab8cf3
vebradev <justinas@vebra.dev>
2020-03-04 10:42:30 +0000

    initial setup

--- util/create_world.py
+++ util/create_world.py
@@ -5,10 +5,10 @@ from adventure.models import Player, Room
 Room.objects.all().delete()
 
 r_outside = Room(title="Outside Cave Entrance",
-               description="North of you, the cave mount beckons")
+                 description="North of you, the cave mount beckons")
 
-r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
-passages run north and east.""")
+r_foyer = Room(
+    title="Foyer", description="""Dim light filters in from the south. Dusty passages run north and east.""")
 
 r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -40,8 +40,7 @@ r_narrow.connectRooms(r_foyer, "w")
 r_narrow.connectRooms(r_treasure, "n")
 r_treasure.connectRooms(r_narrow, "s")
 
-players=Player.objects.all()
+players = Player.objects.all()
 for p in players:
-  p.currentRoom=r_outside.id
-  p.save()
-
+    p.currentRoom = r_outside.id
+    p.save()
